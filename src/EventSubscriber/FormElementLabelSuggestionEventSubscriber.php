daf5413ccd6eaaca857ae49090738b7e6962ca76
Gert van den Buijs <gert.vandenbuijs@tactics.be>
2022-05-31 11:09:55 +0200

    Add form element label suggestions

--- src/EventSubscriber/FormElementLabelSuggestionEventSubscriber.php
+++ src/EventSubscriber/FormElementLabelSuggestionEventSubscriber.php
@@ -4,6 +4,7 @@ namespace Drupal\suggestions\EventSubscriber;
 
 use Drupal\core_event_dispatcher\Event\Theme\ThemeSuggestionsAlterIdEvent;
 use Drupal\preprocess_event_dispatcher\Event\AbstractPreprocessEvent;
+use Drupal\preprocess_form_element_event_dispatcher\Event\FormElementPreprocessEvent;
 use Drupal\suggestions\Service\SuggestionBuilder;
 use Drupal\suggestions\Service\SuggestionCleaner;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -20,7 +21,26 @@ class FormElementLabelSuggestionEventSubscriber implements EventSubscriberInterf
   public static function getSubscribedEvents() {
     return [
       'hook_event_dispatcher.theme.suggestions_form_element_label_alter' => 'provideSuggestions',
+      FormElementPreprocessEvent::name() => 'addFormElementType'
     ];
+  }
+
+  /**
+   * Preprocess form element to add parent type for suggestions to label.
+   *
+   * This is a small hack until Drupal uses the correct consistent way of theming
+   * to add label to the form element. Now we just preprocess the form element and
+   * pass the type of the parent along in an array key.
+   *
+   * @param FormElementPreprocessEvent $event
+   *   The event.
+   *
+   * @return void
+   */
+  public function addFormElementType(FormElementPreprocessEvent $event): void
+  {
+    $variables = $event->getVariables();
+    $variables->getByReference('label')['type'] = $variables->get('type');
   }
 
   /**
@@ -31,7 +51,8 @@ class FormElementLabelSuggestionEventSubscriber implements EventSubscriberInterf
    *
    * @return void
    */
-  public function provideSuggestions(ThemeSuggestionsAlterIdEvent $event) {
+  public function provideSuggestions(ThemeSuggestionsAlterIdEvent $event): void
+  {
 
     $suggestions = &$event->getSuggestions();
 
@@ -43,10 +64,13 @@ class FormElementLabelSuggestionEventSubscriber implements EventSubscriberInterf
     $builder = \Drupal::service('suggestions.builder');
 
     $variables = $event->getVariables();
-    /*
 
-    // Add suggestion : form_element_label__TYPE.
-    $suggestions[] = $builder->build([$event->getHook(), $element['#type']]);
+    // Small hack to read out parent type. (@see addFormElementType)
+    $type = $variables['type'] ?? NULL;
+    if ($type) {
+      // Add suggestion : form_element_label__TYPE.
+      $suggestions[] = $builder->build([$event->getHook(), $type]);
+    }
 
     // Add suggestion based on extra attribute on input so we can provide an
     // form_element based template suggestion. This should only be used for
@@ -59,7 +83,7 @@ class FormElementLabelSuggestionEventSubscriber implements EventSubscriberInterf
         $suggestions[] = $builder->build([$event->getHook(), $customTwigSuggestion]);
        }
     }
-    */
+
     $cleaner->unique($suggestions);
   }
 }
